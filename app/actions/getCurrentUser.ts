895c7e3066e817e6065bfed71943d3c8057e3263
gunil0610 <gunil.na1@gmail.com>
2023-04-14 17:52:14 +0900

    fix: sanitize user type from prisma

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -19,7 +19,16 @@ export default async function getCurrentUser() {
       where: { email: session.user.email },
     });
 
-    return currentUser;
+    if (!currentUser) {
+      return null;
+    }
+
+    return {
+      ...currentUser,
+      createdAt: currentUser.createdAt.toISOString(),
+      updatedAt: currentUser.updatedAt.toISOString(),
+      emailVerified: currentUser.emailVerified?.toISOString() || null,
+    };
   } catch (error: any) {
     return null;
   }
